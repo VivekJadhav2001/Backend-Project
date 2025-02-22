eeb9fefc620e33db13b8f6a449520183cf4234f6
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-02-22 12:59:51 +0530

    add access & refresh Token, login-logout user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,25 @@ import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
+
+
+const generateAccessAndRefereshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        user.save({validateBeforeSave: false})
+
+        return{accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "something went wrong while genrating referesh and access token")
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
 
     // get user details from frontend
@@ -94,6 +113,74 @@ const registerUser = asyncHandler(async (req, res) => {
     )
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req, res) => {
+
+    // req body -> data
+    // username or email
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie
+
+    // req body -> data
+    const {email, username, password} = req.body
+    // username or email
+    if (!username || !email){
+        throw new ApiError(400, "username or email is required")
+    }
+
+    //find the user
+    const user = await User.findOne({
+        $or: [{username}, {email}]            //mongoDB operators
+    })
+
+    if(!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+    //password check
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+    //access and referesh token
+    //we create a method for this by the name generateAccessAndRefereshToken. check above the page
+
+
+    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    //send cookie
+    const options = {
+        httpOnly : true,
+        secure: true
+    }
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshtoken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user:loggedInUser, accessToken, refreshToken
+            },
+            "User Logged in Successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async(req, res) => {
+    
+})
+
+
+
+export {
+    registerUser,
+    loginUser
+}
 
 
