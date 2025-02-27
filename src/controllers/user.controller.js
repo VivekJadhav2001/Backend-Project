a4f3a105a4d9e2e404507bcf2ca5099cd336bae3
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-02-27 13:09:41 +0530

    fix: remove trailing whitespace in getCurrentUser function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -278,7 +278,7 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
         .json(new ApiResponse(200, req.user, "current user fetched successfully"))
-})
+}) 
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
     const { fullName, email } = req.body
